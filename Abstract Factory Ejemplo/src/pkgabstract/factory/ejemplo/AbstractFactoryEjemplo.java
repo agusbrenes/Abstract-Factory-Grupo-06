12449703042575a9c145f55fa42a5b4fa38f8f13
agusbrenes <56280607+agusbrenes@users.noreply.github.com>
2021-04-14 19:29:31 -0600

    Cambios del main

--- Abstract Factory Ejemplo/src/pkgabstract/factory/ejemplo/AbstractFactoryEjemplo.java
+++ Abstract Factory Ejemplo/src/pkgabstract/factory/ejemplo/AbstractFactoryEjemplo.java
@@ -1,21 +1,27 @@
 package pkgabstract.factory.ejemplo;
 
+import java.util.Scanner;
+
 public class AbstractFactoryEjemplo {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which type of Figures do you wish for?\n1: Rounded\n2: Normal");
+        int type = scanner.nextInt();
+        boolean isRounded = (type == 1) ? true : false;
         // Ejemplos de Shapes normales
-        AbstractFactory shapeFactory = FactoryProducer.getFactory(false);
+        AbstractFactory shapeFactory = FactoryProducer.getFactory(isRounded);
         Shape rectangle = shapeFactory.getShape("RECTANGLE");
         Shape square = shapeFactory.getShape("SQUARE");
         rectangle.draw();
         square.draw();
         
         // Ejemplos de Rounded Shapes
-        AbstractFactory roundShapeFactory = FactoryProducer.getFactory(true);
+        /*AbstractFactory roundShapeFactory = FactoryProducer.getFactory(true);
         Shape roundRectangle = roundShapeFactory.getShape("RECTANGLE");
         Shape roundSquare = roundShapeFactory.getShape("SQUARE");
         roundRectangle.draw();
-        roundSquare.draw();      
+        roundSquare.draw();  */    
     }
 
 }
