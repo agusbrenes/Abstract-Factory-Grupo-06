d13226390ea9605e16ad6274055515f27e4a2efa
agusbrenes <56280607+agusbrenes@users.noreply.github.com>
2021-04-14 18:38:33 -0600

    Inicio de Proyecto Netbeans con Shapes

---
+++ Abstract Factory Ejemplo/src/pkgabstract/factory/ejemplo/AbstractFactoryEjemplo.java
@@ -0,0 +1,9 @@
+package pkgabstract.factory.ejemplo;
+
+public class AbstractFactoryEjemplo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ Abstract Factory Ejemplo/src/pkgabstract/factory/ejemplo/RoundedRectangle.java
@@ -0,0 +1,10 @@
+package pkgabstract.factory.ejemplo;
+
+public class RoundedRectangle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Inside RoundedRectangle::draw() method.");
+    }
+
+}
